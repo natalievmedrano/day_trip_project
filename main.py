5e0a7db3c53cf394f399341c7daabbf84659b6c1
natalievmedrano <nataliemed0403@gmail.com>
2022-12-09 15:14:06 -0600

    Created Lists on Main py file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ("hello world")
+#Store the trip options for destinations, restaurants, transportation, and entertainment each in their own List (this would be good to place at the top of the file)
+#Get a random element from each of those sets of options. We recommend declaring variables for a random destination, restaurant, transportation, and entertainment
+#Display the initial random trip to your user
+#Prompt the user to see if they are satisfied
+#If not, find out which trip feature they want to change and randomly select a new feature.
+#Keep doing this process until the user is satisfied with the trip
+
+
+
+destinations=['Bahamas','New York City,NY','Atlanta,GA',"Washington,D.C","Austin,TX","Las Vegas NV"]
+resturants=['Mcdonalds','Cheesecake Factory','Taco Cabana','Cheddars','Wings n More','Texas Road House']
+transportations=['train','car','plane','bus','walking','swimming']
+entertainments=['casino','museum','movies','shopping','theme park','kayaking']
+
+
+
+    
+
