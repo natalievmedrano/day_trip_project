f0572748884c4fc56d992d26232cc29e8b3bdad1
natalievmedrano <nataliemed0403@gmail.com>
2022-12-12 16:51:42 -0600

    fixed up code so that program will run smoothly just need a few more adjustments

--- main.py
+++ main.py
@@ -5,58 +5,75 @@
 #If not, find out which trip feature they want to change and randomly select a new feature.
 #Keep doing this process until the user is satisfied with the trip
 
-greeting_message= "WELCOME TO 'MY U.S.A DAY TRIP GENERATOR' WE LOOK FORWARD TO HELP YOU PLAN THE VACATION OF YOUR DREAMS"
+greeting_message= "WELCOME TO 'MY U.S.A DAY TRIP GENERATOR' WE LOOK FORWARD TO HELP YOU PLAN THE U.S.A VACATION OF YOUR DREAMS"
 print(greeting_message)
+second_greeting="Here is the trip we have chosen for you!"
+print(second_greeting)
 
 #variables
 usa_destinations=['New York City,NY','Atlanta,GA',"Washington,D.C","Austin,TX","Las Vegas NV","Los Angeles,CA"]
-usa_resturants=['Mcdonalds','Cheesecake Factory','Taco Cabana','Cheddars','Wings n More','Texas Road House']
-usa_transportations=['train','car','plane','bus','walking','swimming']
-usa_entertainments=['casino','museum','movies','shopping','theme park','kayaking']
+usa_resturants=['Tacos Locos','Crab Shack','Blue Dynasty','BurgerFi','Wings n More','Texas Road House']
+usa_transportations=['Train','Car','Plane','Bus','Boat','Swimming']
+usa_entertainments=['Casino','Museum','Movies','Shopping','Theme park','Kayaking']
 
 #selected things/choices for user
 import random
-def choice(self, seq):
-    destinations= random.choice(usa_destinations)
-    resturants= random.choice(usa_resturants)
-    transportations= random.choice(usa_transportations)
-    entertainments= random.choice(usa_entertainments)
 
-#second greeting
+destinations= random.choice(usa_destinations)
+resturants= random.choice(usa_resturants)
+transportations= random.choice(usa_transportations)
+entertainments= random.choice(usa_entertainments)
 
-second_greeting = 'Here is the first selected trip for you!'
-print(second_greeting)
-
-# when program is ran these will be the first ones chosen
-chosen_destination= 'For your destination: {destination}'
-chosen_resturant= 'For refreshments and meals: {resturants}'
-chosen_transportation= 'For transportation: {transportations}'
-chosen_entertainment= 'For entertainment: {entertainments}'
-
-print('ARE YOU SATISFIED WITH THIS TRIP? please state yes or no.')
-
+print(f'For your U.S.A Destination:{destinations}',f'For your meals:{resturants}',f'For your transportation:{transportations}',f'For your entertainment:{entertainments}')
 
 #allow user to confirm or deny trip
+print(f'Are you satisfied with these selections?')
 user_confirmation= input('Type YES or NO to confirm.')
 
 #if user isnt satisfied with the selections...
-print("Thank you for your input! Let's change that really quick which part of your trip would you like to change?")
 
-#destinations= print()
-#testurants=print()
-#transportations= print()
-#entertainments= print()
-#i think i have to create many elifs for this part 
+def new_usa_destination(user_confirmation):
+        while user_confirmation != 'YES':
+            chosen_destination= random.choice(usa_destinations)
+            user_confirmation= input(f"Is {destinations} a more suitable choice for you?")
+        else:
+            print(f"You chose {destinations} as your Destination!")
 
-#print("ARE YOU HAPPY WITH YOUR U.S.A VACATION? Y OR N")
-#here i have to create a boolean i think
+    
+new_usa_destination(user_confirmation)
 
-#print (WHICH PART OF YOUR TRIP WOULD YOU LIKE TO CHANGE?)
-# for this part we need to use an input function
 
-#print ('ANYTHING ELSE YOU WOULD LIKE TO CHANGE Y OR N)
 
-#print('CONFIRM TRIP BY SAYING YES OR NO')
+def new_usa_resturant (user_confirmation):
 
-#print("")
+        while user_confirmation != 'YES':
+            chosen_resturant= random.choice(usa_resturants)
+            user_confirmation= input(f"Sorry you were not satisfied with our choice, is {resturants} a more suitable choice?")
+        else:
+            print(f'Awesome! you have selected {resturants}.')
 
+new_usa_resturant(user_confirmation)
+
+
+
+def new_usa_transportation (user_confirmation):
+        while user_confirmation != 'YES':
+            chosen_transportation= random.choice(usa_transportations)
+            user_confirmation= input(f"Sorry you weren't happy with our choices,is {transportations} a more suitable choice?")
+        else:
+            print(f'You chose {transportations} as your transportation method!')
+
+new_usa_transportation(user_confirmation)
+
+
+
+def new_usa_entertainment(user_confirmation):
+        while user_confirmation != 'YES':
+            chosen_entertainment= random.choice(usa_entertainments)
+            user_confirmation= input(f"Sorry you weren't satisfied with our choice")
+        else:
+            print(f'You have chosen {entertainments} as your entertainment!')
+
+new_usa_entertainment(user_confirmation)
+
+print("We hope you enjoy your trip! ENJOY!!!!!!!")
